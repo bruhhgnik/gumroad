3b6350587bae4af1c2c24745f6f91cdad952ee0f
Ershad Kunnakkadan <ershad92@gmail.com>
2026-02-05 13:15:44 -0500

    Implement feature toggle for Careers pages (#3370)
    Related to #2487

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -3,6 +3,7 @@
 class CareersController < ApplicationController
   layout "home"
 
+  before_action { e404 if Feature.inactive?(:career_pages) }
   before_action :set_body_class
   before_action :set_meta_data
 
